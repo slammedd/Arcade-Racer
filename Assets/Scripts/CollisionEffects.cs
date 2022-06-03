72a76ec296e7b5820a38d904d8b3dccc419f3659
Kyle James <kyleajames102@gmail.com>
2022-06-03 14:38:01 +0100

    Implemented screenshake on impact.

--- Assets/Scripts/CollisionEffects.cs
+++ Assets/Scripts/CollisionEffects.cs
@@ -1,16 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class CollisionEffects : MonoBehaviour
 {
     public ParticleSystem impactEffect;
     public AudioSource impactSource;
     public AudioClip impactSound;
+    public CinemachineVirtualCamera vCam;
+ 
+
+    private float shakeTimer;
 
     private void OnCollisionEnter(Collision collision)
     {
         Instantiate(impactEffect, collision.GetContact(0).point, Quaternion.identity);
         impactSource.PlayOneShot(impactSound);
+        ScreenShake(5f, 0.1f);
+    }
+
+    public void ScreenShake(float intensity, float time)
+    {
+        CinemachineBasicMultiChannelPerlin cinemachineBasicMultiChannelPerlin = vCam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+
+        cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = intensity;
+        shakeTimer = time;
+    }
+
+    private void Update()
+    {
+        if(shakeTimer > 0)
+        {
+            shakeTimer -= Time.deltaTime;
+            if(shakeTimer <= 0f)
+            {
+                CinemachineBasicMultiChannelPerlin cinemachineBasicMultiChannelPerlin = vCam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+
+                cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = 0f;
+            }
+        }
     }
 }
